9a63e7871b6c3a701c1d82bdac8fe8c711392887
Shuelong Moua <moua_shuelong@yahoo.com>
2020-10-27 01:43:22 -0500

    updated roles table with foreign key

--- employee-tracker.sql
+++ employee-tracker.sql
@@ -7,12 +7,14 @@ use employee_trackerDB;
 CREATE TABLE department (
   department_id int NOT NULL auto_increment,
   name varchar(30) NOT NULL,
-  PRIMARY KEY(id)
+  PRIMARY KEY(department_id)
 );
 
 CREATE TABLE roles (
-  department_id int NOT NULL auto_increment,
+  role_id int NOT NULL auto_increment,
   title varchar(30) NOT NULL,
   salary DECIMAL(7,2),
-  PRIMARY KEY(department_id)
+  department_id int NOT NULL,
+  PRIMARY KEY(role_id),
+  FOREIGN KEY (department_id) REFERENCES department(department_id)
 );
