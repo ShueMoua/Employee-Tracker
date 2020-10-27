160f94987b62c36307f124cae126bebcb1553dc9
Shuelong Moua <moua_shuelong@yahoo.com>
2020-10-27 00:17:20 -0500

    updated id in dpt table to department_id

--- employee-tracker.sql
+++ employee-tracker.sql
@@ -5,7 +5,7 @@ create database employee_trackerDB;
 use employee_trackerDB;
 
 CREATE TABLE department (
-  id int NOT NULL auto_increment,
+  department_id int NOT NULL auto_increment,
   name varchar(30) NOT NULL,
   PRIMARY KEY(id)
 );
